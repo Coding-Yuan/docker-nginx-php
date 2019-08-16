58918caed89cae9ef3b4a7890d39d4ca19869bf2
Yuanjm <1348989525@qq.com>
2019-08-16 21:28:46 +0800

    chore: 补充日志目录
    nginx-php.sh脚本优化

--- my.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-WUID=`ls /data/wwwroot -dl | awk '{print $3}'`
-
-if [ $WUID != 'www' ];then
-   usermod -u $WUID www
-fi
--- work/nginx-php.sh
+++ work/nginx-php.sh
@@ -1,9 +1,12 @@
 #/bin/bash
 
-WUID=`ls /data/wwwroot -dl | awk '{print $3}'`
-WGID=`ls /data/wwwroot -dl | awk '{print $4}'`
+VDIR="/data/wwwroot"
+VUSER="www"
+
+WUID=`ls $VDIR -dl | awk '{print $3}'`
+WGID=`ls $VDIR -dl | awk '{print $4}'`
 # 目录属户属组为数字时更改
 if [ -n "$(echo $WUID| sed -n "/^[0-9]\+$/p")" ];then
-   usermod -u $WUID www
-   groupmod -g $WGID www
+   usermod -u $WUID $VUSER
+   groupmod -g $WGID $VUSER
 fi
